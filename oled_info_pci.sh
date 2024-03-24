0d8d67642f7b6851816f4206b1d5d93405dd482f
DroZDi <121637367+drozdi70@users.noreply.github.com>
2024-03-24 13:10:48 +0100

    ..

--- oled_info_pci.sh
+++ oled_info_pci.sh
@@ -6,7 +6,12 @@ cd /nvme/work/oled
 
 #Send data to OLED
 ./oled128x64 /dev/i2c-1
-sleep 30
+sleep 20
+rm info2.log
+#Send data sensors to OLED
+./info_pci2.sh
+./oled128x64 /dev/i2c-1
+sleep 20
 rm info2.log
 #Clear OLED display
 ./oled128x64 /dev/i2c-1
