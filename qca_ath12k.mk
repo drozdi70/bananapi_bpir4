adca918da92b570fff103569957099dbe248b330
DroZDi <121637367+drozdi70@users.noreply.github.com>
2024-05-25 13:32:06 +0200

    Add files via upload
    firmware

--- qca_ath12k.mk
+++ qca_ath12k.mk
@@ -1,8 +1,8 @@
-Package/ath12k-firmware-wnc7850 = $(call Package/firmware-default,WNC7850 ath12k firmware,,LICENCE.atheros_firmware)
-define Package/ath12k-firmware-wnc7850/install
-	$(INSTALL_DIR) $(1)/lib/firmware/ath12k/WNC7850/hw2.0
+Package/ath12k-firmware-wcn7850 = $(call Package/firmware-default,WNC7850 ath12k firmware,,LICENCE.atheros_firmware)
+define Package/ath12k-firmware-wcn7850/install
+	$(INSTALL_DIR) $(1)/lib/firmware/ath12k/WCN7850/hw2.0
 	$(INSTALL_DATA) \
-		$(PKG_BUILD_DIR)/ath12k/WNC7850/hw2.0/* $(1)/lib/firmware/ath12k/WNC7850/hw2.0/
+		$(PKG_BUILD_DIR)/ath12k/WCN7850/hw2.0/* $(1)/lib/firmware/ath12k/WCN7850/hw2.0/
 endef
-$(eval $(call BuildPackage,ath12k-firmware-wnc7850))
+$(eval $(call BuildPackage,ath12k-firmware-wcn7850))
 
